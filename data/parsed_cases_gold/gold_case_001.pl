8388a3c813e50f3bb38a020dbf254d341ba3f7fc
ni-eck <62644108+ni-eck@users.noreply.github.com>
2025-06-30 23:47:45 +0200

    Add files via upload

---
+++ data/parsed_cases_gold/gold_case_001.pl
@@ -0,0 +1,8 @@
+% Case facts
+financial_situation_after(pcc, grant, 66995312, 0, 0, 0, 0, 0).
+direct_transfer(pcc, grant).
+subsidy(grant).
+resource_of_central_state(generalbudgetpoland, grant).
+active_spending(grant).
+granting_entity(grant, polishministryofeconomy).
+public_authority(polishministryofeconomy, poland).
---
+++ data/parsed_cases_gold/gold_case_002.pl
@@ -0,0 +1,8 @@
+% Case facts
+financial_situation_after(pcc, tax_exemption, 0, 0, 28802370, 0, 0, 0).
+direct_transfer(pcc, tax_exemption).
+tax_exemption(tax_exemption).
+resource_of_central_state(generalbudgetpoland, tax_exemption).
+exemptions_reductions_taxes(tax_exemption).
+granting_entity(tax_exemption, polishministryofeconomy).
+public_authority(polishministryofeconomy, poland).
